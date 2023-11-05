396be2d7fefa55ded6105afd6cc91573985fa56e
falita123 <mrsqueex@gmail.com>
2023-11-05 17:34:04 -0600

    updated twitch page

--- src/app/twitch/page.tsx
+++ src/app/twitch/page.tsx
@@ -1,29 +1,29 @@
-"use client"
+// "use client"
 
-import SearchUser from '@/components/twitch/SearchTwitchUser';
-// import { db } from '@/lib/db';
-import axios from 'axios';
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import { useState } from 'react';
+// import SearchUser from '@/components/twitch/SearchTwitchUser';
+// // import { db } from '@/lib/db';
+// import axios from 'axios';
+// import { useSession } from 'next-auth/react';
+// import Image from 'next/image';
+// import { useState } from 'react';
 
-export default function TwitchPage() {
-  const { data: session } = useSession();
-  const [search, setSearch] = useState('');
+// export default function TwitchPage() {
+//   const { data: session } = useSession();
+//   const [search, setSearch] = useState('');
 
-  if (!session) {
-    return <div>Loading...</div>;
-  }
+//   if (!session) {
+//     return <div>Loading...</div>;
+//   }
 
-  return (
-    <div className="flex items-center space-x-4">
-      <Image className="h-10 w-10 rounded-full" src={session?.user?.image || '/placeholder.svg'} alt="User image" width={100} height={100}/>
-      <div>
-        <div className="font-bold">{session.user.name}</div>
-        <div className="text-sm text-gray-500">{session.user.email}</div>
-        <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
-        <SearchUser userId={session.user.id} search={search} />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="flex items-center space-x-4">
+//       <Image className="h-10 w-10 rounded-full" src={session?.user?.image || '/placeholder.svg'} alt="User image" width={100} height={100}/>
+//       <div>
+//         <div className="font-bold">{session.user.name}</div>
+//         <div className="text-sm text-gray-500">{session.user.email}</div>
+//         <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+//         <SearchUser userId={session.user.id} search={search} />
+//       </div>
+//     </div>
+//   );
+// }
