ab6b1ea3828fd019dfadfb8dcaf86c3cffafb286
falita123 <mrsqueex@gmail.com>
2023-11-05 17:30:21 -0600

    updating

--- src/components/twitch/SearchTwitchUser.tsx
+++ src/components/twitch/SearchTwitchUser.tsx
@@ -1,49 +1,49 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { db } from '@/lib/db';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { db } from '@/lib/db';
 
-export default function SearchUser({ userId, search }: { userId: string, search: string }) {
-  // const [data, setData] = useState(null);
+// export default function SearchUser({ userId, search }: { userId: string, search: string }) {
+//   // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const getAccessToken = async () => {
-      const account = await db.account.findFirst({
-        where: {
-          userId: userId,
-          provider: 'twitch',
-        },
-      });
+//   useEffect(() => {
+//     const getAccessToken = async () => {
+//       const account = await db.account.findFirst({
+//         where: {
+//           userId: userId,
+//           provider: 'twitch',
+//         },
+//       });
 
-      return account?.access_token;
-    };
+//       return account?.access_token;
+//     };
 
-    const fetchData = async () => {
-      const access_token = await getAccessToken();
+//     const fetchData = async () => {
+//       const access_token = await getAccessToken();
 
-      const response = await axios.get('https://api.twitch.tv/helix/users', {
-        headers: {
-          'Client-ID': process.env.TWITCH_CLIENT_ID,
-          'Authorization': `Bearer ${access_token}`
-        },
-        params: {
-          login: search
-        }
-      });
+//       const response = await axios.get('https://api.twitch.tv/helix/users', {
+//         headers: {
+//           'Client-ID': process.env.TWITCH_CLIENT_ID,
+//           'Authorization': `Bearer ${access_token}`
+//         },
+//         params: {
+//           login: search
+//         }
+//       });
 
-      return (response.data);
-    };
+//       return (response.data);
+//     };
 
-    fetchData();
-  }, [userId, search]);
+//     fetchData();
+//   }, [userId, search]);
 
-  // if (!data) {
-  //   return <div>Loading...</div>;
-  // }
+//   // if (!data) {
+//   //   return <div>Loading...</div>;
+//   // }
 
-  // Render the data here
-  return (
-    <div>
-      {/* Render the data here */}
-    </div>
-  );
-}
+//   // Render the data here
+//   return (
+//     <div>
+//       {/* Render the data here */}
+//     </div>
+//   );
+// }
